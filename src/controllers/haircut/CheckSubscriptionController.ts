d60f7986ca43131a7b99720115e0e4285a02f200
unknown <matheusfariasdasneves@gmail.com>
2023-12-31 01:35:04 -0300

    feat: Adicionar funcionalidade que verifica se o usuario tem uma assinatura ou nao

---
+++ src/controllers/haircut/CheckSubscriptionController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { CheckSubscriptionService } from '../../services/haircut/CheckSubscriptionService';
+
+class CheckSubscriptionController{
+  async handle(req: Request, res: Response){
+    const user_id = req.user_id;
+
+    const checkSubscriptionService = new CheckSubscriptionService();
+
+    const status = await checkSubscriptionService.execute({
+      user_id,
+    });
+
+    return res.json(status);
+  };
+};
+
+export { CheckSubscriptionController };
