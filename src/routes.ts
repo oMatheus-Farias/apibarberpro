f941489998a940e6d2b6ae970d29fecab8499666
unknown <matheusfariasdasneves@gmail.com>
2023-12-31 00:13:35 -0300

    feat: Adicionar funcionalidade que cria modelos de corte

--- src/routes.ts
+++ src/routes.ts
@@ -7,11 +7,15 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { UpdateUserController } from './controllers/user/UpdateUserController';
 
+import { CreateHaircutController } from './controllers/haircut/CreateHaircutController';
+
 import { isAuthenticated } from './middlewares/isAuthenticared';
 
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 router.put('/users', isAuthenticated, new UpdateUserController().handle);
+
+router.post('/haircut', isAuthenticated, new CreateHaircutController().handle);
 
 export { router };
